863992aeb03d70a11df59cd70a22e9df6668ec1f
telamonian <telamonian@users.noreply.github.com>
2024-06-28 13:07:10 -0400

    first draft of `Appler` in `install.py`

--- install.py
+++ install.py
@@ -1,76 +1,162 @@
+import os
 from pathlib import Path
 import subprocess
 import sys
+from textwrap import dedent
 
-rocmPytorchUrl = 'https://download.pytorch.org/whl/rocm6.0'
-topName = 'requirements_top'
+PathLike = os.PathLike[str] | str
 
-cmdCompileTop = [sys.executable, '-m', 'uv', 'pip', 'compile', '-q', f'{topName}.txt', '-o', f'{topName}.lock', '--override', 'overrides.txt']
-cmdInstallTop = [sys.executable, '-m', 'uv', 'pip', 'install', '--extra-index-url', rocmPytorchUrl, '-r', f'{topName}.lock', '--no-deps']
+def run(cmd: list[str], cwd: str = ".") -> str:
+    return subprocess.run(
+        cmd,
+        cwd=cwd,
+        capture_output=True,
+        text=True,
+    )
 
-def handleOpencv(p: Path):
-    """as per the opencv docs, you should only have exactly one opencv package.
-    headless is more suitable for comfy than the gui version, so remove gui if
-    headless is present. TODO: add support for contrib pkgs. see: https://github.com/opencv/opencv-python"""
-    
-    with open(p / f'{topName}.lock', 'r') as f:
-        lines = f.readlines()
-    
-    guiFound, headlessFound = False, False
-    for line in lines:
-        if 'opencv-python==' in line:
-            guiFound = True
-        elif 'opencv-python-headless==':
-            headlessFound = True
-
-    if headlessFound and guiFound:
-        with open(p / f'{topName}.lock', 'w') as f:
-            for line in lines:
-                if 'opencv-python==' not in line:
-                    f.write(line)
-
-def run(cmd: list[str], cwd: str):
+def check_call(cmd: list[str], cwd: str):
     """uses check_call to run pip, as reccomended by the pip maintainers.
     see https://pip.pypa.io/en/stable/user_guide/#using-pip-from-your-program"""
 
     subprocess.check_call(cmd, cwd=cwd)
 
-def makeTop(p: Path):
-    """write a top-level requirements file that links to the requirements of
-    comfyui and all installed custom nodes"""
+class Appler:
+    rocmPytorchUrl = "https://download.pytorch.org/whl/rocm6.0"
+    nvidiaPytorchUrl = "https://download.pytorch.org/whl/cu121"
 
-    reqs = p.glob('custom_nodes/[!__pycache__]*/requirements.txt')
+    overrideGpu = dedent("""
+        # ensure usage of {gpu} version of pytorch
+        --extra-index-url {gpuUrl}
+        torch
+        torchsde
+        torchvision
+    """).strip()
 
-    with open(p / f'{topName}.txt', 'w') as f:
-        f.write('# ensure usage of amd/rocm version of pytorch\n')
-        f.write(f'--extra-index-url {rocmPytorchUrl}\n\n')
-        f.write('# main comfy ui dependencies\n')
-        f.write('-r requirements.txt\n\n')
-        f.write('# custom node dependencies\n')
-        for req in reqs:
-            f.write(f'-r {req}\n')
+    reqNames = [
+        "requirements.txt",
+        "pyproject.toml",
+        "setup.cfg",
+        "setup.py",
+    ]
 
-        f.write('\n')
-
-def compileTop(p: Path):
-    """resolve the top-level requirements into a "lock" file using `uv pip compile`
-    (see https://github.com/astral-sh/uv?tab=readme-ov-file#limitations)"""
+    @staticmethod
+    def findReqFiles(p: PathLike) -> list[Path]:
+        p = Path(p).absolute()
+        reqFiles: list[Path] = []
+        for reqName in Appler.reqNames:
+            reqFiles.extend(p.glob(reqName))
+        return reqFiles
     
-    # first, clean up
-    (p / f'{topName}.lock').unlink(missing_ok=True)
-    run(cmdCompileTop, str(p))
+    # cmdCompileTop = [sys.executable, "-m", "uv", "pip", "compile", "-q", f"{topName}.txt", "-o", f"{topName}.lock", "--override", "overrides.txt"]
+    # cmdInstallTop = [sys.executable, "-m", "uv", "pip", "install", "--extra-index-url", rocmPytorchUrl, "-r", f"{topName}.lock", "--no-deps"]
 
-    # dedupe opencv
-    handleOpencv(p)
+    @staticmethod
+    def compile(reqFiles: list[PathLike], override: PathLike | None = None, out: PathLike | None = None) -> str:
+        cmd = [
+            sys.executable,
+            "-m",
+            "uv",
+            "pip",
+            "compile",
+        ]
+        
+        for reqFile in reqFiles:
+            cmd.append(str(reqFile))
 
-def installTop(p: Path):
-    """install the resolved top-level requirements using `uv pip install`"""
+        if override is not None:
+            cmd.extend([
+                "--override",
+                str(override),
+            ])
+        
+        if out is not None:
+            cmd.extend([
+                "-o",
+                str(out),
+            ])
 
-    run(cmdInstallTop, str(p))
+        return run(cmd)
 
-if __name__ == '__main__':
-    here = Path('.')
+    def __init__(
+        self,
+        cwd: PathLike = ".",
+        extDirs: list[PathLike] = [],
+        gpu: str | None = None,
+        outName: str = "requirements.compiled",
+    ):
+        self.cwd = Path(cwd)
+        self.extDirs = [Path(extDir) for extDir in extDirs] if extDirs is not None else None
+        self.gpu = gpu
 
-    makeTop(here)
-    compileTop(here)
-    installTop(here)
+        self.gpuUrl = Appler.nvidiaPytorchUrl if self.gpu == "nvidia" else Appler.rocmPytorchUrl if self.gpu == "amd" else None
+        self.out = self.cwd / outName
+        self.override = self.cwd / "override.txt"
+
+        self.coreReqFiles = Appler.findReqFiles(self.cwd)
+        self.extReqFiles = [reqFile for extDir in self.extDirs for reqFile in Appler.findReqFiles(extDir)]
+
+    def makeOverride(self):
+        with open(self.override, "w") as f:
+            if self.gpu is not None:
+                f.write(Appler.overrideGpu.format(gpu=self.gpu, gpuUrl=self.gpuUrl))
+                f.write("\n\n")
+            f.write("# ensure that core comfyui deps take precedence over any 3rd party extension deps\n")
+
+        coreOverride = Appler.compile(self.coreReqFiles, override=self.override)
+
+        with open(self.override, "a") as f:
+            for line in coreOverride:
+                f.write(line)
+
+    def compileCorePlusExt(self):
+        Appler.compile(
+            reqFiles=(self.coreReqFiles + self.extReqFiles),
+            override=self.override,
+            out=self.out,
+        )
+
+    def handleOpencv(self):
+        """as per the opencv docs, you should only have exactly one opencv package.
+        headless is more suitable for comfy than the gui version, so remove gui if
+        headless is present. TODO: add support for contrib pkgs. see: https://github.com/opencv/opencv-python"""
+        
+        with open(self.out, "r") as f:
+            lines = f.readlines()
+        
+        guiFound, headlessFound = False, False
+        for line in lines:
+            if "opencv-python==" in line:
+                guiFound = True
+            elif "opencv-python-headless==" in line:
+                headlessFound = True
+
+        if headlessFound and guiFound:
+            with open(self.out, "w") as f:
+                for line in lines:
+                    if "opencv-python==" not in line:
+                        f.write(line)
+
+def installComfyDeps(cwd: PathLike, gpu: str):
+    p = Path(cwd)
+    extDirs = [d for d in p.glob("custom_nodes/[!__pycache__]*") if d.is_dir()]
+    
+    appler = Appler(cwd=cwd, extDirs=extDirs, gpu=gpu)
+
+    appler.makeOverride()
+    appler.compileCorePlusExt()
+    appler.handleOpencv()
+
+# def installTop(p: Path):
+#     """install the resolved top-level requirements using `uv pip install`"""
+
+#     run(cmdInstallTop, str(p))
+
+# if __name__ == "__main__":
+#     here = Path(".")
+
+#     makeTop(here)
+#     compileTop(here)
+#     installTop(here)
+
+if __name__ == "__main__":
+    installComfyDeps(cwd=".", gpu="amd")
